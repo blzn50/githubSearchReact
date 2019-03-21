38f8af51c690ffc4e63edbebb3967e9f14802648
naren <blazeraze.naren@gmail.com>
2019-03-21 23:26:15 +0200

    updated search box sizing in small screen

--- src/components/Header.js
+++ src/components/Header.js
@@ -3,20 +3,20 @@ import { Navbar, NavbarToggler, NavbarBrand } from 'reactstrap';
 
 class Header extends React.Component {
   state = {
-    isOpen: false
+    isOpen: false,
   };
 
   toggle() {
     this.setState({
-      isOpen: !this.state.isOpen
+      isOpen: !this.state.isOpen,
     });
   }
 
   render() {
     return (
       <div>
-        <Navbar color="dark" expand="md" className="mb-5">
-          <NavbarBrand href="/">Github Search</NavbarBrand>
+        <Navbar color="secondary" expand="md" className="mb-5">
+          <NavbarBrand href="/">Github Profile Search</NavbarBrand>
           <NavbarToggler onClick={this.toggle} />
           {/* <Collapse isOpen={this.state.isOpen} navbar>
             <Nav className="ml-auto" navbar>
