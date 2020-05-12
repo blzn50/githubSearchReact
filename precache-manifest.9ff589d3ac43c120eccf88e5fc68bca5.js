59867dc3b28e1c8c0734d8738c2efc4cb3f6bdf7
naren <blazeraze.naren@gmail.com>
2020-05-12 15:13:19 +0545

    Updates

---
+++ precache-manifest.9ff589d3ac43c120eccf88e5fc68bca5.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9556f2c16261e5796d639c2344f124ea",
+    "url": "/githubSearchReact/index.html"
+  },
+  {
+    "revision": "c9f233711fdd040d6c46",
+    "url": "/githubSearchReact/static/css/2.0161871a.chunk.css"
+  },
+  {
+    "revision": "027526827d99f7255230",
+    "url": "/githubSearchReact/static/css/main.e19ce1c4.chunk.css"
+  },
+  {
+    "revision": "c9f233711fdd040d6c46",
+    "url": "/githubSearchReact/static/js/2.2c33960c.chunk.js"
+  },
+  {
+    "revision": "a24e643e070412f6063089357e56bec2",
+    "url": "/githubSearchReact/static/js/2.2c33960c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "027526827d99f7255230",
+    "url": "/githubSearchReact/static/js/main.c7f4d3ad.chunk.js"
+  },
+  {
+    "revision": "2cc8ea82de53fdb3e762",
+    "url": "/githubSearchReact/static/js/runtime-main.6bf8107c.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -11,13 +11,19 @@
  * See https://goo.gl/2aRDsh
  */
 
-importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/githubSearchReact/precache-manifest.2f548b5875174ad3d9cc2110c76f9ee3.js"
+  "/githubSearchReact/precache-manifest.9ff589d3ac43c120eccf88e5fc68bca5.js"
 );
 
-workbox.clientsClaim();
+self.addEventListener('message', (event) => {
+  if (event.data && event.data.type === 'SKIP_WAITING') {
+    self.skipWaiting();
+  }
+});
+
+workbox.core.clientsClaim();
 
 /**
  * The workboxSW.precacheAndRoute() method efficiently caches and responds to
@@ -25,10 +31,9 @@ workbox.clientsClaim();
  * See https://goo.gl/S9QRab
  */
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
-workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/githubSearchReact/index.html", {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/githubSearchReact/index.html"), {
   
-  blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
+  blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
 });
